83da6877b7ee66948b74adf32bc7074251be1215
unknown <teddy1565>
2020-12-21 00:30:28 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CornerStoneDemo' });
+});
+router.get('/images',(req,res,next)=>{
+  let data=fs.readFileSync("D:\\TeddyXiong\\Emecca\\example_DICOM_image\\DICOM_IMAGE\\20500999\\SDY00000\\SRS00000\\IMG00049.DCM");
+  res.send(data);
+});
+
+module.exports = router;
