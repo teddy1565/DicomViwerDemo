701d03bb555b69596c5a5923d4fcf8f1f9119434
unknown <teddy1565>
2020-12-21 01:03:05 +0800

    add decodeTask

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CornerStoneDemo' });
 });
 router.get('/images',(req,res,next)=>{
-  let data=fs.readFileSync("D:\\TeddyXiong\\Emecca\\example_DICOM_image\\DICOM_IMAGE\\20500999\\SDY00000\\SRS00000\\IMG00049.DCM");
+  //let data=fs.readFileSync("D:\\TeddyXiong\\Emecca\\example_DICOM_image\\DICOM_IMAGE\\20500999\\SDY00000\\SRS00000\\IMG00049.DCM");
+  let data=fs.readFileSync("D:\\TeddyXiong\\Emecca\\example_DICOM_image\\DICOM_IMAGE\\41386161\\SDY00000\\SRS00000\\IMG00000.DCM");
   res.send(data);
 });
 
