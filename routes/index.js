f2ec5ec87fe3426f0515a302e145107414d88c2e
teddy1565 <teddy1565@gmail.com>
2020-12-21 11:45:10 +0800

    mac modify

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CornerStoneDemo' });
 });
 router.get('/images',(req,res,next)=>{
+  let {Study,series,num} = req.query;
   //let data=fs.readFileSync("D:\\TeddyXiong\\Emecca\\example_DICOM_image\\DICOM_IMAGE\\20500999\\SDY00000\\SRS00000\\IMG00049.DCM");
-  let data=fs.readFileSync("D:\\TeddyXiong\\Emecca\\example_DICOM_image\\DICOM_IMAGE\\41386161\\SDY00000\\SRS00000\\IMG00000.DCM");
+  //let data=fs.readFileSync("D:\\TeddyXiong\\Emecca\\example_DICOM_image\\DICOM_IMAGE\\41386161\\SDY00000\\SRS00000\\IMG00000.DCM");
+  let data=fs.readFileSync(`/Users/zhenkaixiong/Emecca/DicomDEMO/DicomViwerDemo/DICOM/${Study}/SDY00000/SRS${series}/IMG${num}.DCM`);
+  //let data=fs.readFileSync("/Users/zhenkaixiong/Emecca/DicomDEMO/DicomViwerDemo/DICOM/41386161/SDY00000/SRS00000/IMG00000.DCM");
   res.send(data);
 });
 
